2982522115f93229b7b58dc7127b19ff60160c2c
CeCamillo <cesarcamillo2212@gmail.com>
2023-04-28 22:06:59 -0300

    Web scraper started

--- scripts/scraper.ts
+++ scripts/scraper.ts
@@ -1,14 +1,14 @@
 import { PGEssay } from "@/types";
 import axios from "axios"; // Handle HTTP Request
 import * as cheerio from "cheerio"; // Parse HTML
-import { encode } from "gpt-3-encoder"; 
+import { encode } from "gpt-3-encoder";
 
 const BASE_URL = "http://www.paulgraham.com";
 
 const getLinks = async () => {
     const html = await axios.get(`${BASE_URL}/articles.html`);
-    const $ = cheerio.load(html.data);  
-    
+    const $ = cheerio.load(html.data);
+
     const tables = $("table");
 
     const linkArr: { url: string, title: string }[] = [];
@@ -19,18 +19,18 @@ const getLinks = async () => {
 
             links.each((i, link) => {
 
-            const url = $(link).attr("href");
-            const title = $(link).text();
+                const url = $(link).attr("href");
+                const title = $(link).text();
 
-            if (url && url.endsWith(".html")) {
-                const linkObj = {
-                    url,
-                    title
+                if (url && url.endsWith(".html")) {
+                    const linkObj = {
+                        url,
+                        title
+                    }
+
+                    linkArr.push(linkObj);
                 }
-
-                linkArr.push(linkObj);
-            }
-        });
+            });
         }
     });
 
@@ -38,7 +38,7 @@ const getLinks = async () => {
 }
 
 
-const getEssay = async (url: string, title: string ) => {
+const getEssay = async (url: string, title: string) => {
     let essay: PGEssay = {
         title: "",
         url: "",
@@ -47,25 +47,56 @@ const getEssay = async (url: string, title: string ) => {
         tokens: 0,
         chunks: []
     }
-    
+
     const html = await axios.get(`${BASE_URL}/${url}`);
     const $ = cheerio.load(html.data);
     const tables = $("table");
 
-    const text = $("tables").text();
-
-    let cleannedText = text.replace(/\s+/g, " ").replace(/\.([a-zA-Z])/g, ". $1");
-
     tables.each((i, table) => {
         if (i === 1) {
             const text = $(tables).text();
+
+            let cleannedText = text.replace(/\s+/g, " ").replace(/\.([a-zA-Z])/g, ". $1");
+
+            const split = cleannedText.match(/[A-Z][a-z]* [0-9]{4}/);
+
+            let dateStr = "";
+            let textWithoutDate = "";
+
+            if (split) {
+                dateStr = split[0];
+                textWithoutDate = cleannedText.replace(dateStr, "");
+            }
+
+            let textEssay = textWithoutDate.replace(/\n/g, " ").trim();
+
+            essay = {
+                title,
+                url: `${BASE_URL}/${url}`,
+                date: dateStr,
+                content: textEssay,
+                tokens: encode(textEssay).length,
+                chunks: []
+            }
         }
     });
+
+    return essay
 }
 
 
 (async () => {
     const links = await getLinks();
-    console.log(links);
+
+    let essays: PGEssay[] = [];
+
+    for (let i = 0; i < links.length; i++) {
+        const link = links[i];
+        const essay = await getEssay(link.url, link.title);
+        essays.push(essay)
+        console.log(essay);
+    }
+
+    
 })();
 
